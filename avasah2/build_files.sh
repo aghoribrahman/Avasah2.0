ec1a71f421bc15275cc6f14cbc7bc1c1fe323c68
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-03-03 18:02:46 +0530

    second commit

--- avasah2/build_files.sh
+++ avasah2/build_files.sh
@@ -1,9 +1,4 @@
-echo "Building the Project..."
-python3.9 -m pip install -r requiremnts.txt
-
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Collecting Static..."
+echo " BUILD START"
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD ENDS"
